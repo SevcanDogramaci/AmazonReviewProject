b3dc6938d39304074dded754c8054cbc60cd8f74
SevcanDogramaci <sevcan.dog@gmail.com>
2020-05-08 16:15:15 +0300

    Class conversions are made

--- database_top.py
+++ database_top.py
@@ -1,125 +1,26 @@
-import sqlite3
-from sqlite3 import Error
-
+from database_access import *
+import os
 
 db_file_to_open = r"amazon_reviews_us_shoes_v1_00.db"
+db_file_to_open = os.getcwd() + '\data\\' + db_file_to_open
 db_file_to_save = r"amazon_reviews_us_shoes_v1_00_2015_top10000.db"
 
+db_access = DatabaseAccess(db_file_to_open)
+reviews = db_access.retrive_reviews_with_products_top_n(100)
 
-def create_connection(db_file):
+db_creator = DatabaseCreator(db_file_to_save)
+db_creator.create_table("Product", db_creator.product_columns)
+db_creator.create_table("Review", db_creator.review_columns)
+
+# insert data
+for index, review in reviews.iterrows():
+    #print(row['c1'], row['c2'])
+    product_info = (review[10], review[11], review[12])
+    review_info = (review[1], review[2], review[3], 
+                   review[4], review[5], review[6], 
+                   review[7], review[8], review[9])
+    # insert product
+    db_creator.insert_product(product_info)
+    # insert review
+    db_creator.insert_review(review_info)
     
-    conn = None
-    
-    try:
-        conn = sqlite3.connect(db_file)
-        print("Connected to database!")
-    except Error as e:
-        print("Cannot connect to database!")
-        
-    return conn
-
-
-
-def create_table(conn, sql_query):
-    
-    try:
-        c = conn.cursor()
-        c.execute(sql_query)
-        print("Table created!")
-    except Error as e:
-        print("Table NOT created! - " + str(e))
-
-
-def execute_sql_query(conn, sql_query):
-    result = None
-
-    try:
-        c = conn.cursor()
-        c.execute(sql_query)
-        result = c.fetchall()
-        print("Query executed!")
-    except Error as e:
-        print("Query NOT executed! - " + str(e))
-        
-    return result
-
-def insert_product(conn, review_info):
-    
-    sql_insert_product = """INSERT INTO Product VALUES(?, ?, ?)"""
-    
-    try:
-        cur = conn.cursor()
-        cur.execute(sql_insert_product, review_info)
-        print("Product inserted!")
-    except Error as e:
-        print("Product NOT inserted! - " + str(e))
-
-
-
-def insert_review(conn, review_info):
-    
-    sql_insert_review = """INSERT INTO Review(marketplace, customer_id, product_id, 
-                                            star_rating, helpful_votes, verified_purchase, 
-                                            review_headline, review_body, review_date) 
-                                            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)"""
-    
-    try:
-        cur = conn.cursor()
-        cur.execute(sql_insert_review, review_info)
-        print("Review inserted!")
-    except Error as e:
-        print("Review NOT inserted! - " + str(e))
-
-
-    
-sql_create_product_table = """CREATE TABLE IF NOT EXISTS Product (
-                                    product_id text PRIMARY KEY CHECK(length(product_id) = 10),
-                                    product_title text NOT NULL,
-                                    product_category text NOT NULL
-                                )"""
-
-sql_create_review_table = """CREATE TABLE IF NOT EXISTS Review (
-                                review_id integer PRIMARY KEY AUTOINCREMENT,
-                                marketplace text NOT NULL DEFAULT 'US' CHECK(length(marketplace) = 2),
-                                customer_id text NOT NULL,
-                                product_id text NOT NULL CHECK(length(product_id) = 10),
-                                star_rating integer NOT NULL DEFAULT 0 CHECK(star_rating < 6),
-                                helpful_votes integer NOT NULL DEFAULT 0,
-                                verified_purchase text NOT NULL CHECK(length(verified_purchase) = 1),
-                                review_headline text NOT NULL,
-                                review_body text NOT NULL,
-                                review_date text NOT NULL,
-                                FOREIGN KEY (product_id) REFERENCES Product (product_id) 
-                            )"""
-    
-db_conn = create_connection(db_file_to_open)
-
-reviews = []
-with db_conn:
-    # get data
-    reviews = execute_sql_query(db_conn, "SELECT * FROM Review INNER JOIN Product on Product.product_id = Review.product_id WHERE substr(review_date,1,4) = '2015' LIMIT 10000")
-
-db_conn = create_connection(db_file_to_save)
-
-with db_conn:
-    # enable foreign keys integrity
-    db_conn.execute("PRAGMA foreign_keys = 1")
-    #execute_sql_query(db_conn, "DROP TABLE IF EXISTS Review")
-    #execute_sql_query(db_conn, "DROP TABLE IF EXISTS Product")
-
-    # create Product table
-    create_table(db_conn, sql_create_product_table)
-
-    # create Review table
-    create_table(db_conn, sql_create_review_table)
-
-    # insert data
-    for review in reviews:
-        product_info = (review[10], review[11], review[12])
-        review_info = (review[1], review[2], 
-                        review[3], review[4], review[5], review[6], 
-                        review[7], review[8], review[9])
-        # insert product
-        insert_product(db_conn, product_info)
-        # insert review
-        insert_review(db_conn, review_info)
--- preprocessor.py
+++ preprocessor.py
@@ -1,82 +1,68 @@
-from database import*
+from database_access import DatabaseAccess
 import pandas as pd
-    
-db_file = r"amazon_reviews_us_shoes_v1_00_2015_top10000.db"
 
-def get_wordnet_pos(pos_tag):
-    from nltk.corpus import wordnet
-    
-    if pos_tag.startswith('J'):
-        return wordnet.ADJ
-    elif pos_tag.startswith('V'):
-        return wordnet.VERB
-    elif pos_tag.startswith('N'):
-        return wordnet.NOUN
-    elif pos_tag.startswith('R'):
-        return wordnet.ADV
-    else:
-        return wordnet.NOUN
-    
-def clear_text(text):
-    from nltk.corpus import wordnet
-    from nltk import pos_tag
-    from nltk.corpus import stopwords
-    from nltk.stem import WordNetLemmatizer
-    import string
-    import nltk
-    
-    # convert to lower case
-    text = text.lower()
-    
-    # tokenize text
-    text = nltk.word_tokenize(text)
-    
-    # part-of-speech tagging
-    pos_tags = pos_tag(text)
-    
-    # stemming
-    lemmatizer = WordNetLemmatizer()     
-    text = [lemmatizer.lemmatize(tag[0], get_wordnet_pos(tag[1])) for tag in pos_tags]
-    #text = [lemmatizer.lemmatize(tag[0], get_wordnet_pos(tag[1])) for tag in pos_tags if tag[1] is wordnet.NOUN]
-    #text = [lemmatizer.lemmatize(tag[0], get_wordnet_pos(tag[1])) for tag in pos_tags if tag[1].startswith('N')]
-    
-    # remove stopwords
-    stopwords_en = stopwords.words('english')
-    text = [word for word in text if word not in stopwords_en]
-    
-    # remove punctuation
-    text = [word.strip(string.punctuation) for word in text]
-    
-    # remove numbers
-    text = [word for word in text if not any(letter.isdigit() for letter in word)]
+class Preprocessor:
+    @staticmethod
+    def __get_wordnet_pos(pos_tag):
+        from nltk.corpus import wordnet
+
+        if pos_tag.startswith('J'):
+            return wordnet.ADJ
+        elif pos_tag.startswith('V'):
+            return wordnet.VERB
+        elif pos_tag.startswith('N'):
+            return wordnet.NOUN
+        elif pos_tag.startswith('R'):
+            return wordnet.ADV
+        else:
+            return wordnet.NOUN
         
-    # remove empty and one-letter words
-    text = [word for word in text if len(word) > 1]
+    @staticmethod
+    def clear_sentence(text):
+        from nltk.stem import WordNetLemmatizer
+        from nltk.corpus import wordnet
+        from nltk.corpus import stopwords
+        from nltk import pos_tag
+        import string
+        import nltk
+    
+        # convert to lower case
+        text = text.lower()
 
-    return " ".join(text)
+        # tokenize text
+        text = nltk.word_tokenize(text)
 
-def main():
-    import os
-    db_conn = create_connection(os.getcwd() + '\data\\' + db_file)
+        # part-of-speech tagging
+        pos_tags = pos_tag(text)
 
-    # get reviews
-    sql_select_all_reviews  = "SELECT * FROM Review"
-    reviews = execute_sql_query(db_conn, sql_select_all_reviews)
+        # stemming
+        lemmatizer = WordNetLemmatizer()     
+        #text = [lemmatizer.lemmatize(tag[0], Preprocessor.__get_wordnet_pos(tag[1])) for tag in pos_tags]
+        #text = [lemmatizer.lemmatize(tag[0], Preprocessor.__get_wordnet_pos(tag[1])) for tag in pos_tags if tag[1] is wordnet.NOUN]
+        text = [lemmatizer.lemmatize(tag[0], Preprocessor.__get_wordnet_pos(tag[1])) for tag in pos_tags if tag[1].startswith('N')]
 
-    # form data frame of reviews for manipulation
-    data_frame = pd.DataFrame(reviews)
-    data_frame.columns = ['review_id', 'marketplace', 'customer_id', 'product_id', 'rate', 
-                        'helpful_votes', 'purchased', 'review_head', 'review_body', 'date']
+        # remove stopwords
+        stopwords_en = stopwords.words('english')
+        text = [word for word in text if word not in stopwords_en]
 
-    # clean reviews
-    data_frame['review_head'] = [clear_text(text) for text in data_frame['review_head']]
-    data_frame['review_body'] = [clear_text(text) for text in data_frame['review_body']]
+        # remove punctuation
+        text = [word.strip(string.punctuation) for word in text]
 
-    print(data_frame['review_head'])
+        # remove numbers
+        text = [word for word in text if not any(letter.isdigit() for letter in word)]
 
-    print(data_frame['review_body'])
+        # remove empty and one-letter words
+        text = [word for word in text if len(word) > 1]
 
+        return " ".join(text)
+    
+    @staticmethod
+    def clear_doc(doc):
+        clean_doc = []
 
-if __name__ == '__main__':
-    main()
+        for sentence in doc:
+            clean_sentence = Preprocessor.clear_sentence(sentence)
+            if len(clean_sentence) > 2:
+                clean_doc.append(clean_sentence)
 
+        return clean_doc
