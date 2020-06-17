030610030e2c36d421d6d3753a8f0f885b2a8d3a
mustafacataltas1 <mustafa.cataltas@ogr.gidatarim.edu.tr>
2020-06-17 13:14:19 +0300

    sentence seperation

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from database_access import DatabaseAccess
-from preprocess import Preprocessor
+from preprocessor import Preprocessor
 from extracter_analyzer import *
 from plotter import Plotter
 from dbscan import DbScan
 import os
 
+
 def get_reviews():
-    db_file = r"amazon_reviews_us_shoes_v1_00_2015_top10000_bad.db"
+    db_file = r"amazon_reviews_us_Watches_v1_00_2015_top10000.db"
     db_file = os.getcwd() + '\data\\' + db_file
 
     db = DatabaseAccess(db_file)
@@ -16,7 +17,7 @@ def get_reviews():
 def perform_tf_idf_and_print(min_df=0.05, max_df=0.9):
     print("\n<----- Tf Idf Starts ----->")
     tf_idf_review_bodys = get_tfidf(
-        [str(sen) for sen in review_bodys], min_df=min_df, max_df=max_df)
+        [sen[0] for sen in cleaned_review_bodys], min_df=min_df, max_df=max_df)
 
     # print results
     print(tf_idf_review_bodys)
@@ -24,16 +25,18 @@ def perform_tf_idf_and_print(min_df=0.05, max_df=0.9):
 
 
 def perform_db_scan_and_print(data, original_data, review_bodys, min_samples_val, eps_val, dataset_name):
-    from collections import Counter 
-    
+    from collections import Counter
+
     print("\n<----- Db Scan Starts ----->")
-    
+
     dbscan = DbScan()
-    dbres = dbscan.perform_db_scan(data, min_samples_val, eps_val)  # inside dbscan.py
-    
+    dbres = dbscan.perform_db_scan(
+        data, min_samples_val, eps_val)  # inside dbscan.py
+
     plotter = Plotter()
-    title = dataset_name + " - eps: " + str(eps_val) + ", min_samples: " + str(min_samples_val)
-    plotter.plot_cluster(dbres, data, title) # inside plotter.py
+    title = dataset_name + " - eps: " + \
+        str(eps_val) + ", min_samples: " + str(min_samples_val)
+    plotter.plot_cluster(dbres, data, title)  # inside plotter.py
 
     labels = dbres.labels_
     # print number of elements in each cluster
@@ -42,14 +45,14 @@ def perform_db_scan_and_print(data, original_data, review_bodys, min_samples_val
 
     # find and print dbscan result on actual text data - review_bodys
     for i in set(labels):
-        if i != -1: # do not print if noise (-1)
+        if i != -1:  # do not print if noise (-1)
             print(i, "----")
             for x in range(len(data)):
                 if labels[x] == i:
-                    print(">>>", (review_bodys[x])[0])
-                    original_review_id = (review_bodys[x])[1]
-                    print(original_data[original_review_id])    
-                    
+                    print(">>>", (review_bodys[x]))
+                    #original_review_id = (review_bodys[x])[1]
+                    #print(original_data[original_review_id])
+
 # --- MAIN Starts
 
 
@@ -59,7 +62,7 @@ dataset_name = "SHOES"
 # get reviews from database
 data_frame = get_reviews()
 
-review_bodys = list(data_frame["review_body"])
+original_review_bodys = list(data_frame["review_body"])
 
 preproc = Preprocessor()
 
@@ -67,15 +70,16 @@ preproc = Preprocessor()
 #review_bodys = preproc.split_sentences(reviews=review_bodys)
 
 # filter reviews according to sentiment analysis results
-original_review_bodys = polarize_reviews_body(200, review_bodys)
-print("Row number after polarity score: ", len(review_bodys))
+# original_review_bodys = polarize_reviews_body(200, review_bodys)
+# print("Row number after polarity score: ", len(review_bodys))
 
 data_frame = None  # to free memory space
 
 # clear reviews body with preprocessing, inside preprocessing.py
-review_bodys = preproc.clear_reviews(original_review_bodys)
+cleaned_review_bodys = preproc.clear_reviews(
+    original_review_bodys, data_size=1000)
 
-print(review_bodys)
+print(cleaned_review_bodys)
 
 # get top words from reviews body, inside preprocessing.py
 #find_top_words(review_bodys, 5)
@@ -87,7 +91,8 @@ tf_idf_review_bodys = perform_tf_idf_and_print(min_df=1, max_df=0.8)
 #eps = plotter.plot_k_distance(tf_idf_review_bodys, k=2)
 
 # db scan with min_samples and eps (eps observed from k-distance graph)
-perform_db_scan_and_print(tf_idf_review_bodys, original_review_bodys, review_bodys, 2, 1.0, dataset_name)
+perform_db_scan_and_print(
+    tf_idf_review_bodys, original_review_bodys, cleaned_review_bodys, 2, 1.0, dataset_name)
 
 
 # --- MAIN Finishes
