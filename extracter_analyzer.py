9d7a4a4f95b7983de7626e68dcde1c4dcef52e1f
SevcanDogramaci <sevcan.dog@gmail.com>
2020-06-19 22:44:03 +0300

    Common words extraction in a sentence is added

--- extracter_analyzer.py
+++ extracter_analyzer.py
@@ -2,9 +2,23 @@ from re import search
 from nltk import pos_tag
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from collections import Counter
 
 analyser = SentimentIntensityAnalyzer()
 
+def get_common_words(val):
+    s = ""
+    for item in val:
+        s = s + " " + item[0]
+        
+    counter = Counter(s.split())
+    most_occur = counter.most_common(2)
+    
+    if len(most_occur) > 1:
+        if most_occur[0][1] != most_occur[1][1]:
+            most_occur = [most_occur[0]]
+            
+    return most_occur
 
 def find_top_words(data, word_num):
 
@@ -88,79 +102,3 @@ def polarize_reviews_body(data_size, reviews):
             review_bodys.append(reviews[x])
     return review_bodys
 
-# --------------------------------------------------------------------
-
-"""
-s = "DONT BUY THESE!	Do not buy these! They break very fast I spun then for 15 minutes " + \
-    "and the end flew off don't waste your money." + "They are made from cheap plastic and" + \
-    " have cracks in them. Buy the poi balls they work a lot better if you only have limited funds."
-#s = "It comes with a rechargeable battery that does not seem to last all that long, especially if you use the flash a lot."
-#s = "After nearly 800 pictures I have found that this camera takes incredible pictures"
-#s = "The strap is horrible and gets in the way of parts the camera you need access to."
-"""
-
-def find_matched_phrases(phrases, patterns):
-    for pattern in patterns:
-        for chunk in phrases:
-            if search(pattern, chunk[1]) is not None:
-                print("Pattern is ", pattern)
-                print("Ngram: ", chunk[0])
-                print("Postags: ", chunk[1])
-                print("Opinion word: ", search_opinion_word(
-                    chunk[0], chunk[1]))
-                print()
-
-
-def search_opinion_word(phrase, postag):
-    postag = postag[:-1].split(";")
-    tag_id = None
-
-    for tag in postag:
-        if tag == "JJ":
-            tag_id = postag.index(tag)
-            break
-        elif tag == "RB" or tag == "RBR" or tag == "RBS":
-            tag_id = postag.index(tag)
-
-    return phrase[tag_id]
-
-
-def get_words_and_postags(ngrams):
-    words_and_postags = []
-    for ngram in ngrams:
-        ngram = pos_tag(ngram)
-        words = []
-        postags = ""
-
-        for item in ngram:
-            words.append(item[0])
-            postags += (item[1] + ";")
-        words_and_postags.append((words, postags))
-
-    return (words_and_postags)
-
-
-three_element_patterns = ["JJ;(NN|NNS);(NN|NNS);",
-                          "NN;VBZ;JJ;",             # added for test
-                          "(RB|RBR|RBS);(JJ|RB|RBR|RBS);JJ;",
-                          "(RB|RBR|RBS);(JJ|RB|RBR|RBS);(NN|NNS);"]
-two_element_patterns = ["JJ;(NN|NNS);",
-                        "(RB|RBR|RBS);JJ;",
-                        "(VBN|VBD);(NN|NNS);",
-                        "(RB|RBR|RBS);(VBN|VBD);",
-                        "(VBN|VBD);(RB|RBR|RBS);"]
-"""
-two_element_ngrams = get_ngrams(s, 2)
-three_element_ngrams = get_ngrams(s, 3)
-
-find_matched_phrases(get_words_and_postags(
-    two_element_ngrams), two_element_patterns)
-print(">> ---- <<")
-find_matched_phrases(get_words_and_postags(
-    three_element_ngrams), three_element_patterns)
-"""
-"""
-from nltk import FreqDist
-fdist = FreqDist("The strap is horrible and gets in the way of parts the camera you need access to.".split(" "))
-print(fdist.most_common())
-"""
