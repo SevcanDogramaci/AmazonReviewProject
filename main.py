ee306c3e1cbcd5dcf6eb7eaac287920ee24b80ab
SevcanDogramaci <sevcan.dog@gmail.com>
2020-06-19 22:43:13 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from pattern_matcher import PatternMatcher
 from database_access import DatabaseAccess
 from preprocessor import Preprocessor
 from extracter_analyzer import *
 from plotter import Plotter
 from dbscan import DbScan
+
+from collections import Counter
 import os
 
 
@@ -22,65 +25,92 @@ def perform_tf_idf_and_print(min_df=0.05, max_df=0.9):
     # print results
     print(tf_idf_review_bodys)
     return tf_idf_review_bodys
-
-
-def perform_db_scan_and_print(data, original_data, review_bodys, min_samples_val, eps_val, dataset_name):
-    from collections import Counter
-
+   
+    
+def perform_db_scan(data, min_samples_val, eps_val):
     print("\n<----- Db Scan Starts ----->")
 
     dbscan = DbScan()
     dbres = dbscan.perform_db_scan(
-        data, min_samples_val, eps_val)  # inside dbscan.py
+        data, min_samples_val, eps_val)  
+    
+    return dbres
+    
+def plot_db_scan(dbres, data, min_samples_val, eps_val, dataset_name):
+    plotter = Plotter()
+    title = dataset_name + " - eps: " + \
+        str(eps_val) + ", min_samples: " + str(min_samples_val)
+    plotter.plot_cluster(dbres, data, title) 
+    
+
+def extract_results(labels, original_data, review_bodys):
+    # print number of elements in each cluster
+    cluster_counts = Counter(labels)
+    print(cluster_counts)
+
+    clusters = {}
+    pattern_matcher = PatternMatcher()
+    # find and print dbscan result on actual text data - review_bodys
+    for i in set(labels):
+        if i != -1:  # do not print if noise (-1)
+            clusters[i] = []
+            print(i, "----")
+            for x in range(len(review_bodys)):
+                if labels[x] == i:
+                    
+                    print(">>>", (review_bodys[x]))
+                    
+                    sentence = get_review(review_bodys[x])
+                    matches = pattern_matcher.find_matches(sentence)
+                    clusters[i].append((review_bodys[x][0], sentence, matches))
+                    
+                    print(clusters[i], "\n")
+    print(clusters)  
+    pattern_matcher.extract_objects(clusters)
+
+def perform_db_scan_and_print(data, original_data, review_bodys, min_samples_val, eps_val, dataset_name):
+    
+    print("\n<----- Db Scan Starts ----->")
+
+    dbscan = DbScan()
+    dbres = dbscan.perform_db_scan(
+        data, min_samples_val, eps_val)  
 
     plotter = Plotter()
     title = dataset_name + " - eps: " + \
         str(eps_val) + ", min_samples: " + str(min_samples_val)
-    plotter.plot_cluster(dbres, data, title)  # inside plotter.py
+    plotter.plot_cluster(dbres, data, title) 
 
     labels = dbres.labels_
     # print number of elements in each cluster
     cluster_counts = Counter(labels)
     print(cluster_counts)
 
-    clusters = []
+    clusters = {}
+    pattern_matcher = PatternMatcher()
     # find and print dbscan result on actual text data - review_bodys
     for i in set(labels):
         if i != -1:  # do not print if noise (-1)
-            clusters.append([])
+            clusters[i] = []
             print(i, "----")
             for x in range(len(data)):
                 if labels[x] == i:
-                    clusters[i].append(review_bodys[x])
+                    
                     print(">>>", (review_bodys[x]))
-                    find_nouns(get_review(review_bodys[x]))
-    
-    return clusters
-
+                    
+                    sentence = get_review(review_bodys[x])
+                    matches = pattern_matcher.find_matches(sentence)
+                    clusters[i].append((review_bodys[x][0], sentence, matches))
+                    
+                    print(clusters[i], "\n")
+    print(clusters)  
+    pattern_matcher.extract_objects(clusters)
+                      
+                   
 def get_review(sentence_tuple):
     review = preproc.split_review_into_sentences(original_review_bodys[sentence_tuple[1]])
     return review[sentence_tuple[2]]
 
-def find_nouns(sentence):
-    import spacy
-    nlp = spacy.load("en_core_web_sm")
-
-    sentence = nlp(sentence)
-    # iterate over nouns in sentence
-    print("Review: ", sentence)
-    for chunk in sentence.noun_chunks:
-        print("Noun chunk: ", chunk.text)
-        print("Noun: ", chunk.root.text)
-        print()
-        
-    """
-    for token in sentence:
-        #print(token, "-", token.pos_)
-        print("Token:" , token)
-        for child in token.rights:
-            print("Child -> ", child.text)
-            print("\n")
-    """
 
 # --- MAIN Starts
 
@@ -106,7 +136,7 @@ data_frame = None  # to free memory space
 
 # clear reviews body with preprocessing, inside preprocessing.py
 cleaned_review_bodys = preproc.clear_reviews(
-    original_review_bodys, data_size=1000)
+    original_review_bodys, data_size=500)
 
 print(cleaned_review_bodys)
 
@@ -120,8 +150,13 @@ tf_idf_review_bodys = perform_tf_idf_and_print(min_df=1, max_df=0.8)
 #eps = plotter.plot_k_distance(tf_idf_review_bodys, k=2)
 
 # db scan with min_samples and eps (eps observed from k-distance graph)
-perform_db_scan_and_print(
-    tf_idf_review_bodys, original_review_bodys, cleaned_review_bodys, 2, 1.0, dataset_name)
+min_samples_val = 3
+eps_val = 1.0
+db_res = perform_db_scan(tf_idf_review_bodys, min_samples_val, eps_val)
+plot_db_scan(db_res, tf_idf_review_bodys, min_samples_val, eps_val, dataset_name)
+extract_results(db_res.labels_, original_review_bodys, cleaned_review_bodys)
+# perform_db_scan_and_print(
+#     tf_idf_review_bodys, original_review_bodys, cleaned_review_bodys, 3, 1.0, dataset_name)
 
 
 # --- MAIN Finishes
