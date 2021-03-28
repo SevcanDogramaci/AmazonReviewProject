bfa512154c8f1f3ad5666aee2b24cd6b1269f8aa
Mustafa Çataltaş <mustafacataltas@yandex.com>
2021-03-28 16:34:55 +0300

    Remove unused parts

--- dbscan.py
+++ dbscan.py
@@ -1,5 +1,4 @@
 from sklearn.cluster import DBSCAN
-#import hdbscan
 
 
 class DbScan:
@@ -16,15 +15,3 @@ class DbScan:
         print("Silhouette Coefficient: %0.3f" %
               metrics.silhouette_score(data, labels))
         return clusters
-
-    """def perform_hdb_scan(self, data):
-        from sklearn.datasets import make_blobs
-
-        data, _ = make_blobs(1000)
-
-        clusterer = hdbscan.RobustSingleLinkage(cut=0.125, k=7)
-        cluster_labels = clusterer.fit_predict(data)
-        hierarchy = clusterer.cluster_hierarchy_
-        alt_labels = hierarchy.get_clusters(0.100, 5)
-        hierarchy.plot()
-        return cluster_labels"""
